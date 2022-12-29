e8b71f0e8ddccd4b95a4688c6a4095fe22b9275a
StephenKennedy5 <5nehpets5kennedy@gmail.com>
2022-12-28 17:01:10 -0800

    added change sizes

--- drawing.js
+++ drawing.js
@@ -1,10 +1,47 @@
 // Creates 9x9 grid
-function make_grid() {
+// Ability to adjust size of grid
+function grid_size() {
+    const small = (16 * 16);
+    const medium = (32 * 32);
+    const large = (64 * 64);
+
+    const buttons = document.querySelectorAll(".size > button");
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.innerHTML === "small") {
+                make_grid(button.innerHTML,small)
+            } else if (button.innerHTML === "medium") {
+                make_grid(button.innerHTML,medium)
+            } else {
+                make_grid(button.innerHTML,large)
+            }
+        })
+    })
+    
+}
+
+// make grid
+function make_grid(size,size_val) {
     const toAdd = document.getElementById('container');
     
     toAdd.innerHTML = '';
+
+    toAdd.classList.remove('lrg','small','med');
+
+    console.log(size);
+
+    if (size === "large") {
+        erase();
+        toAdd.classList.add('lrg');
+    } else if (size === "medium") {
+        erase();
+        toAdd.classList.add('med');
+    } else {
+        erase();
+        toAdd.classList.add('small');
+    }
     
-    for (let i = 0; i < (9*9); i++) {
+    for (let i = 0; i < size_val; i++) {
         const div = document.createElement('div');
         // div.className = 'draw';
         toAdd.appendChild(div);
@@ -39,11 +76,10 @@ function erase() {
         })
     });
 
-
 }
 
 function draw() {
-    make_grid();
+    grid_size();
     drawing();
     erase();
 }
